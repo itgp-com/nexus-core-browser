51bfa1b9dfb4b38d25b2dff26fa16e5b433efede
David Pociu <dpociu@gmail.com>
2022-05-14 18:31:16 -0400

    2.1.03

--- Mirror-to-averba.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-ECHO Sync from CORE TO AVERBA
-pause
-C:\apps\mirror\Mirror64.exe C:\_subversion\trunk\nexus\core\nexus-core-browser\core C:\_subversion\trunk\nexus\customer\ipt\averba-web\averba\node_modules\nexus-core-browser\core
-pause
