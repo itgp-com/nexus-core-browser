065d2cf8becb8a6e1dc41dd00134f320d5a726dd
David Pociu <dpociu@gmail.com>
2022-05-27 12:20:28 -0400

    2.1.21

--- core/gui/controls/AbstractWgtText.ts
+++ core/gui/controls/AbstractWgtText.ts
@@ -4,6 +4,50 @@ import {DataProvider, IDataProviderSimple}                                      
 import {Args_WgtSimple, WgtSimple}                                                from "./WgtSimple";
 import {applyHtmlDecoration, StringArg, stringArgVal}                             from "../../CoreUtils";
 
+export class Args_WgtText_Multiline {
+   /**
+    * (Optional) Initial number of lines. If not specified, defaults to 2
+    */
+   initial_line_count ?: number;
+
+   /**
+    * (Optional) Should the text box autoresize depending on its content. Defaults to yes.
+    */
+   autoresize?: boolean;
+
+   /**
+    * (Optional) This function will be called on the create event of the text box if the autoresize parameter above is set to true.
+    *
+    * This function will replace the default autoresize create function which has the following code:
+    *
+    * <code>
+    * (args: any) => {
+    *    this.obj.addAttributes({rows: "2"});
+    *    this.obj.element.style.height = "auto";
+    *    this.obj.element.style.height = (this.obj.element.scrollHeight - 7) + "px";
+    * }
+    * </code>
+    */
+   autoresize_created_function_override ?: (args:any)=>void;
+
+   /**
+    * (Optional) This function will be called on the input event of the text box if the autoresize parameter above is set to true.
+    *
+    * This function will replace the default autoresize input function which has the following code:
+    *
+    * <code>
+    * (args: any) => {
+    *      args.event.currentTarget.style.height = "auto";
+    *      args.event.currentTarget.style.height = (args.event.currentTarget.scrollHeight) + "px";
+    * }
+    * </code>
+    */
+   autoresize_input_function_override ?: (args:any)=>void;
+
+
+
+} // Args_WgtText_Multiline
+
 export class Args_WgtText extends Args_WgtSimple<TextBoxModel> {
 
    /**
@@ -29,6 +73,12 @@ export class Args_WgtText extends Args_WgtSimple<TextBoxModel> {
 
    enabled ?: boolean;
 
+   /**
+    * (Optional) If this parameter is not null (even if it's {}) then the text box becomes multiline.
+    * By default it will have 2 lines, and will be self-expanding
+    */
+   multiline ?: Args_WgtText_Multiline;
+
 }
 
 /**
@@ -47,8 +97,10 @@ export abstract class AbstractWgtText extends WgtSimple<TextBox, Args_AnyWidget,
 
    initialize_AbstractWgtText(args: Args_WgtText) {
 
-      if (!args.ej)
-         args.ej = {};
+      if (args.ej == null)
+         args.ej = {}; // exists or becomes empty object
+
+      let ej:TextBoxModel = args.ej;
 
       this.args               = args;
       this.stayFocusedOnError = args.stayFocusedOnError;
@@ -66,6 +118,80 @@ export abstract class AbstractWgtText extends WgtSimple<TextBox, Args_AnyWidget,
 
       if (args.enabled != null)
          args.ej.enabled = args.enabled;
+
+
+      if ( args.multiline != null){
+         let autoresize: boolean = args?.multiline?.autoresize;
+         if (autoresize == null)
+            autoresize = true; // default to true
+
+         let initial_line_count : number = args?.multiline?.initial_line_count;
+         if (initial_line_count == null)
+            initial_line_count = 2; // default to 2 lines
+
+         let default_created_function : (ev:any)=>void =(ev: any) => {
+            this.obj.addAttributes({rows: ''+initial_line_count});
+
+            this.obj.element.style.height = "auto";
+            this.obj.element.style.height = (this.obj.element.scrollHeight - 7) + "px";
+         }
+
+         let created_function : (ev:any)=>void = args?.multiline?.autoresize_created_function_override;
+         // if (created_function == null) {
+         //    if (autoresize) {
+         //       created_function = (ev: any) => {
+         //          this.obj.element.style.height = "auto";
+         //          this.obj.element.style.height = (this.obj.element.scrollHeight - 7) + "px";
+         //       }
+         //    }
+         // }
+
+
+         let input_function: (ev:any)=>void = args?.multiline?.autoresize_input_function_override;
+         if (input_function == null){
+            if (autoresize){
+               input_function = (ev: any) => {
+                  ev.event.currentTarget.style.height = "auto";
+                  ev.event.currentTarget.style.height = (ev.event.currentTarget.scrollHeight) + "px";
+               }
+            }
+         }
+
+         //---------- Now let's adjust the default ej passed to the textbox for the above ------------
+         ej.multiline = true;
+
+         let ej_created_function = ej.created;
+         ej.created = (ev:any)=>{
+            try {
+               default_created_function.call(this, ev);
+            } catch(e){ this.handleError(e);}
+
+            if ( created_function != null)
+            try {
+               created_function.call(this, ev);
+            } catch(e){ this.handleError(e);}
+
+            if ( ej_created_function != null)
+               try {
+                  ej_created_function.call(this, ev);
+               } catch(e){ this.handleError(e);}
+         } // ej.created
+
+
+         let ej_input_function = ej.input;
+         ej.input = (ev:any)=>{
+
+            if ( input_function != null)
+               try {
+                  input_function.call(this, ev);
+               } catch(e){ this.handleError(e);}
+
+            if ( ej_input_function != null)
+               try {
+                  ej_input_function.call(this, ev);
+               } catch(e){ this.handleError(e);}
+         } // input
+      } // multiline
 
       this.initialize_WgtSimple(args)
    } // initTextField
