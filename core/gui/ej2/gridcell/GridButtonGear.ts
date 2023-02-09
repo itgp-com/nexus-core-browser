9ebddc125b82631bb6b9dfea13093336ec0dc0c0
itgp-com <dpociu@insitechinc.com>
2023-02-08 19:48:59 -0500

    3.1.15

--- core/gui/ej2/gridcell/GridButtonGear.ts
+++ core/gui/ej2/gridcell/GridButtonGear.ts
@@ -1,31 +1,39 @@
-import {AbstractGridButton}                  from "../abstract/AbstractGridButton";
+import {AbstractGridButton} from "../abstract/AbstractGridButton";
 import {ColumnModel, QueryCellInfoEventArgs} from "@syncfusion/ej2-grids";
-import {GridWidgetCallBack}                  from "../../WidgetUtils";
+import {GridWidgetCallBack} from "../../WidgetUtils";
 
 
 export class GridButtonGear extends AbstractGridButton {
-   static readonly CLASS_NAME:string = 'GridButtonGear';
+    static readonly CLASS_NAME: string = 'GridButtonGear';
 
-   constructor() {
-      super({
-               buttonClass: GridButtonGear.CLASS_NAME,
-               fa_classes:  'fa-solid fa-gear'
-            });
-   }
+    constructor() {
+        super({
+            buttonClass: GridButtonGear.CLASS_NAME,
+            fa_classes: 'fa-solid fa-gear'
+        });
+    }
 }
 
 //----------------------
 const GRID_BUTTON_GEAR: GridButtonGear = new GridButtonGear();
 
-export function gridButtonGearColumnModel(): ColumnModel {
-   return GRID_BUTTON_GEAR.columnModel();
+/**
+ * Get the column model for the gear Grid Button
+ * @param override a ColumnModel object that will override the default values
+ */
+export function gridButtonGearColumnModel(override ?: ColumnModel): ColumnModel {
+    override = override || {};
+    let columnModel: ColumnModel = GRID_BUTTON_GEAR.columnModel();
+    columnModel.field = '_GridButtonGear_'
+    columnModel = Object.assign(columnModel, override);
+    return columnModel;
 }
 
 export function gridButtonGearInstantiate(args: QueryCellInfoEventArgs, callback ?: GridWidgetCallBack, toolTip ?: string) {
-   return GRID_BUTTON_GEAR.instantiate(
-      {
-         args:     args,
-         callback: callback,
-         toolTip:  toolTip,
-      });
+    return GRID_BUTTON_GEAR.instantiate(
+        {
+            args: args,
+            callback: callback,
+            toolTip: toolTip,
+        });
 }
