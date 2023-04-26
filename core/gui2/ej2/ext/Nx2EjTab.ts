a1b160ff2b066329c3491608ebcb79a44978d776
itgp-com <dpociu@insitechinc.com>
2023-04-26 17:35:13 -0400

    3.1.33

---
+++ core/gui2/ej2/ext/Nx2EjTab.ts
@@ -0,0 +1,32 @@
+import {Tab, TabModel} from "@syncfusion/ej2-navigations";
+import {Nx2Evt_OnLogic} from "../../Nx2";
+import {addNx2Class} from '../../Nx2HtmlDecorator';
+import {Nx2EjBasic, StateNx2EjBasic, StateNx2EjBasicRef} from "../Nx2EjBasic";
+
+
+export interface StateNx2EjTabRef extends StateNx2EjBasicRef {
+    widget?: Nx2EjTab;
+}
+
+export interface StateNx2EjTab<WIDGET_LIBRARY_MODEL extends TabModel = TabModel> extends StateNx2EjBasic<WIDGET_LIBRARY_MODEL> {
+    /**
+     * Override with specific type used in code completion
+     * Contains all the fields that have references to this instance and are usually created by the widget initialization code
+     */
+    ref?: StateNx2EjTabRef;
+}
+
+export class Nx2EjTab<STATE extends StateNx2EjTab = StateNx2EjTab> extends Nx2EjBasic<STATE, Tab> {
+    constructor(state ?: STATE) {
+        super(state);
+        addNx2Class(this.state.deco, 'Nx2EjTab');
+    }
+
+
+    onLogic(args: Nx2Evt_OnLogic) {
+        super.onLogic(args);
+
+        this.obj = new Tab(this.state.ej);
+        this.obj.appendTo(this.htmlElement); // this will initialize the htmlElement if needed
+    }
+}
