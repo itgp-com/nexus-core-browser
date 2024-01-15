5e260f8e1db8f568a95ca6210857a07754dfb8f0
dpociu <dpociu@gmail.com>
2024-01-15 14:32:59 -0500

    3.5.67

--- core/gui2/ej2/ext/N2ListBox.ts
+++ core/gui2/ej2/ext/N2ListBox.ts
@@ -1,5 +1,8 @@
 import {ListBox, ListBoxModel} from '@syncfusion/ej2-dropdowns';
+import {cssAddSelector} from '../../../CoreUtils';
+import {N2Evt_AfterLogic} from '../../N2';
 import {addN2Class} from '../../N2HtmlDecorator';
+import {ThemeChangeEvent, themeChangeListeners} from '../../Theming';
 import {N2EjBasic, StateN2EjBasic, StateN2EjBasicRef} from '../N2EjBasic';
 
 
@@ -18,6 +21,7 @@ export interface StateN2ListBox extends StateN2EjBasic<ListBoxModel> {
 
 export class N2ListBox<STATE extends StateN2ListBox = StateN2ListBox> extends N2EjBasic<STATE, ListBox> {
     static readonly CLASS_IDENTIFIER: string = 'N2ListBox';
+    static readonly N2LISTBBOX_WRAPPER: string = `${N2ListBox.CLASS_IDENTIFIER}_Wrapper`;
 
     constructor(state ?: STATE) {
         super(state);
@@ -26,6 +30,27 @@ export class N2ListBox<STATE extends StateN2ListBox = StateN2ListBox> extends N2
     onStateInitialized(state: STATE) {
         addN2Class(state.deco, N2ListBox.CLASS_IDENTIFIER);
         state.deco.tag = 'input';
+        let thisX = this;
+        let f_dataBound = state.ej.dataBound;
+        state.ej.dataBound = (ev: any) => {
+            let e_listbox_wrapper: HTMLElement = this.htmlElementAnchor.closest('.e-listbox-wrapper') as HTMLElement;// parent
+            if (e_listbox_wrapper) {
+                e_listbox_wrapper.classList.remove(N2ListBox.N2LISTBBOX_WRAPPER) // make sure it's not there
+                e_listbox_wrapper.classList.add(N2ListBox.N2LISTBBOX_WRAPPER)
+
+                let select_all_parent: HTMLElement = e_listbox_wrapper.querySelector('.e-selectall-parent');
+                if (select_all_parent) {
+                    let e_list_parent: HTMLElement = e_listbox_wrapper.querySelector('.e-list-parent.e-ul');
+                    e_list_parent.style.height = 'auto';
+                    e_list_parent.style.minHeight = `${row_height}px`;
+                }
+
+            }
+            try {
+                if (f_dataBound) f_dataBound.call(thisX, ev);
+            } catch (e) { console.error(e) }
+
+        } // dataBound
         super.onStateInitialized(state);
     }
 
@@ -36,3 +61,36 @@ export class N2ListBox<STATE extends StateN2ListBox = StateN2ListBox> extends N2
     get classIdentifier(): string { return N2ListBox.CLASS_IDENTIFIER; }
 
 } // main class
+
+const row_height: number = 30;
+
+export function cssForN2ListBox(n2ListBoxClass: string, eListBoxClass: string) {
+    cssAddSelector(`.e-listbox-wrapper.${N2ListBox.N2LISTBBOX_WRAPPER}`, `
+            font-size: var(--app-font-size-regular);`);
+
+    let sel1 = `
+    .${N2ListBox.N2LISTBBOX_WRAPPER}.e-listbox-wrapper:not(.e-list-template) .e-list-item,
+    .${N2ListBox.N2LISTBBOX_WRAPPER}.e-listbox-wrapper .e-list-nrt,
+    .${N2ListBox.N2LISTBBOX_WRAPPER}.e-listbox-wrapper .e-selectall-parent,
+    .${N2ListBox.N2LISTBBOX_WRAPPER} .e-listbox-container:not(.e-list-template) .e-list-item,
+    .${N2ListBox.N2LISTBBOX_WRAPPER} .e-listbox-container .e-list-nrt,
+    .${N2ListBox.N2LISTBBOX_WRAPPER} .e-listbox-container .e-selectall-parent,
+    .${N2ListBox.N2LISTBBOX_WRAPPER} .e-listboxtool-wrapper:not(.e-list-template) .e-list-item,
+    .${N2ListBox.N2LISTBBOX_WRAPPER} .e-listboxtool-wrapper .e-list-nrt,
+    .${N2ListBox.N2LISTBBOX_WRAPPER} .e-listboxtool-wrapper .e-selectall-parent `;
+    cssAddSelector(sel1, `
+        height: ${row_height}px;
+        line-height: 1;
+        padding: 10px 10px;
+        position: relative;`);
+
+    // let sel2 = `.e-listbox-wrapper:has(.e-list-parent.e-ul)`
+    // cssAddSelector(sel2, `
+    //     calc(100% - ${row_height}px)`);
+
+
+} // cssForN2ListBox
+
+themeChangeListeners().add((ev: ThemeChangeEvent) => {
+    cssForN2ListBox(N2ListBox.CLASS_IDENTIFIER, 'e-listbox');
+}); // normal priority
